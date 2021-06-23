0dd401e4ccdd40626ac2943ac43e687913bf0208
qinyuanpei <yuanpei@wangdou-hbean.com>
2021-06-23 12:21:24 +0800

    Add Kafka  Learning

---
+++ src/Kafka.Learning/Kafka.Learning.Consumer/Program.cs
@@ -0,0 +1,55 @@
+﻿using Confluent.Kafka;
+using System;
+using System.Threading;
+
+namespace Kafka.Learning.Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var consumerConfig = new ConsumerConfig
+            {
+                GroupId = "test-consumer-group",
+                BootstrapServers = "192.168.50.162:9092",
+                AutoOffsetReset = AutoOffsetReset.Earliest,
+                AutoCommitIntervalMs = 5000,
+                EnableAutoCommit = true
+            };
+            using (var c = new ConsumerBuilder<Null, string>(consumerConfig).Build())
+            {
+                c.Subscribe("test");
+
+                var cts = new CancellationTokenSource();
+                Console.CancelKeyPress += (_, e) => {
+                    e.Cancel = true;
+                    cts.Cancel();
+                };
+
+                try
+                {
+                    while (true)
+                    {
+                        try
+                        {
+                            var consumeResult = c.Consume(cts.Token);
+                            Console.WriteLine($"Consumed message '{consumeResult.Message.Value}' at: '{consumeResult.TopicPartitionOffset}'.");
+                        }
+                        catch (ConsumeException e)
+                        {
+                            Console.WriteLine($"Error occured: {e.Error.Reason}");
+                        }
+                    }
+                }
+                catch (OperationCanceledException)
+                {
+                    // Ensure the consumer leaves the group cleanly and final offsets are committed.
+                    c.Close();
+                }
+            }
+
+            Console.WriteLine("Send Done!");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ src/Kafka.Learning/Kafka.Learning.Producer/Program.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Confluent.Kafka;
+using System;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Threading;
+
+namespace Kafka.Learning.Producer
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var producerConfig = new ProducerConfig { BootstrapServers = "192.168.50.162:9092" };
+            using (var p = new ProducerBuilder<Null, string>(producerConfig).Build())
+            {
+                try
+                {
+                    for (var i = 1; i <= 100; i++)
+                    {
+                        var student = new { Id = i.ToString("000"), Name = $"Student{i}" };
+                        var result = await p.ProduceAsync("test", new Message<Null, string> { Value = JsonConvert.SerializeObject(student) });
+                        Console.WriteLine($"Delivered '{result.Value}'");
+                    }
+
+                }
+                catch (ProduceException<Null, string> e)
+                {
+                    Console.WriteLine($"Delivery failed: {e.Error.Reason}");
+                }
+            }
+
+            Console.WriteLine("Send Done!");
+            Console.ReadKey();
+        }
+    }
+}
