0f772c246115931ed8f31b33f3f9ea2f415630dd
qinyuanpei <yuanpei@wangdou-hbean.com>
2021-09-22 14:30:21 +0800

    数字证书学习

---
+++ src/CertsLearning/SampleApp/Program.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace SampleApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.ConfigureKestrel(options =>
+                    {
+                        options.ConfigureHttpsDefaults(kestrel =>
+                        {
+                            kestrel.ServerCertificate = new System.Security.Cryptography.X509Certificates.X509Certificate2("./Certificates/Server.pfx","123456789");
+                        });
+
+                        options.Listen(IPAddress.Loopback, 5001, kestrel => kestrel.UseHttps(new X509Certificate2()));
+                    });
+
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
